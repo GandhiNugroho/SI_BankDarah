cc329cc7d60640a2bf17e6b030c49468d94debf4
Husor <asus@LAPTOP-84P58BP4>
2018-11-15 14:02:04 +0700

    SERVICES V 1.4

--- SI_BDservices/src/java/RSS/SI_BDResource.java
+++ SI_BDservices/src/java/RSS/SI_BDResource.java
@@ -5,6 +5,8 @@
  */
 package RSS;
 
+import com.google.gson.Gson;
+import helper.DataDarahHelper;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
@@ -12,7 +14,9 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * REST Web Service
@@ -35,19 +39,26 @@ public class SI_BDResource {
      * Retrieves representation of an instance of RSS.SI_BDResource
      * @return an instance of java.lang.String
      */
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public String getJson() {
+       @GET
+    @Produces(javax.ws.rs.core.MediaType.APPLICATION_JSON)
+    public Response getJson() {
         //TODO return proper representation object
-        throw new UnsupportedOperationException();
+        DataDarahHelper p = new DataDarahHelper();
+        Gson gson = new Gson();
+        return Response.status(Response.Status.OK)
+                .entity(gson.toJson(p.bacaSemuaDataDarah()))
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Methods",
+                        "GET,POST,HEAD,OPTIONS,PUT")
+                .header("Access-Control-Allow-Headers",
+                        "Content-Type,X-Requested-With,accept,Origin,Access-Control-Request-Method,Access-Control-Request-Headers")
+                .header("Access-Exposed-Headers",
+                        "Access-Control-Allow-Origin,Access-Control-Allow-Credentials")
+                .header("Access-Support-Credentials",
+                        "true")
+                .header("Access-Control-Max-Age", "20")
+                .header("Access-Preflight-Maxage", "20")
+                .build();
     }
-
-    /**
-     * PUT method for updating or creating an instance of SI_BDResource
-     * @param content representation for the resource
-     */
-    @PUT
-    @Consumes(MediaType.APPLICATION_JSON)
-    public void putJson(String content) {
-    }
+      
 }
