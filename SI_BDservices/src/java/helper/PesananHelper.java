fa98e400616732809f1f08eb562f6da338c87a9e
Husor <asus@LAPTOP-84P58BP4>
2018-11-20 18:35:18 +0700

    SERVICES V 1.7

---
+++ SI_BDservices/src/java/helper/PesananHelper.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helper;
+
+import Utility.NewHibernateUtil;
+import java.util.Date;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import pojos.Pesanan;
+
+/**
+ *
+ * @author asus
+ */
+public class PesananHelper {
+    public PesananHelper(){
+        
+    }
+
+
+  public void addNewPesanan(
+        int idPesanan,
+     Date tanggalPesan,
+     Date tanggalKirim,
+     Integer idDarah,
+     String golonganDarah,
+     String resus,
+     String pendonor,
+     Date tanggalDonor,
+     String alamatPendonor
+          
+  ){
+         Session session = NewHibernateUtil.getSessionFactory().openSession();
+           Transaction tx = session.beginTransaction();
+           Pesanan pesanan=new Pesanan(idPesanan,tanggalPesan, tanggalKirim, idDarah, golonganDarah, resus, pendonor, tanggalDonor, alamatPendonor);
+           session.saveOrUpdate(pesanan);
+           tx.commit();
+           session.close();
+  }
+  public List<Pesanan> bacaSemuaPesanan() {
+        List<Pesanan> list = null;
+        Session session = NewHibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+        Query q = session.createQuery("from Pesanan p");
+        list = q.list();
+        tx.commit();
+        session.close();
+        return list;
+
+        
+    }
+}
